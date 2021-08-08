5dffc573432b54dd7620f4ecf2d21693866766d5
한석현/고객센터플랫폼개발팀/11ST <hsh0322@sk.com>
2021-08-08 17:27:37 +0900

    [HGI-6] 메인 화면 초기화

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: "Welcome to 한석현's blog"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
